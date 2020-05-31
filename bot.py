051285900051d11986261dd2a7164e44688222b5
Darshan Singh <darshan.singh.tech@gmail.com>
2020-05-31 14:08:19 -0400

    Adding and removing works, sorta
    I can now add and remove the word "orange" from my list at will.
    
    I might port over the lists to a .json file to make it easier to read.

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import random
+import array as arr
 
 silent_answer = ["*shrugs*", "*silence*", "*stoic face combined with silence*", "*annyoed silence*"]
 
@@ -6,8 +7,7 @@ question_answer = ["What can I have to eat?", "When can I get a new book?", "Whe
 
 statement_answer = ["Nah", "No", "One second", "Yeah, let me just finish this first", "Please stop pestering me about this", "Did Mom put you up to this?"]
 
-food_answer = ["I'm good with anything", "Can I have desert?"]
-
+food_answer = ["I'm good with anything", "Can I have desert?", "What can I have to eat?"]
 
 answer = ""
 
@@ -19,6 +19,16 @@ def create_speech():
     elif ((answer in user_statement) and (num == 3)):
         print(question_answer[random.randint(0,(len(question_answer)-1))])
 # This is the function that generates a random answer based on the above parameters
+
+def add_list():
+    question_answer.append("orange")
+    print(question_answer[(len(question_answer))-1])
+    print(question_answer)
+
+def remove_list():
+    question_answer.remove("orange")
+    print(question_answer[(len(question_answer))-1])
+    print(question_answer)
 
 while True:
     num = random.randint(1,3)
@@ -38,7 +48,14 @@ while True:
     create_speech()
     # This handles all statemets with the word "school" in them    
 
-    # answer = "food"
-    # print(food_answer[random.randint(0,(len(food_answer)-1))])
-    # # This if statement handles all questions with the word "food" in them
-    # It also doesn't work
+    if ("food" in user_statement):
+        print(food_answer[random.randint(0,(len(food_answer)-1))])
+    # This if statement handles all questions with the word "food" in them
+
+    if ("add" in user_statement):
+        add_list()
+    # This will write to the above answers
+
+    if ("remove" in user_statement):
+        remove_list()
+    # This will remove one of the above answers
