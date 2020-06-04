9048a5c8ae5f931a95cc284fa7bf9d0b8b45176c
Darshan Singh <darshan.singh.tech@gmail.com>
2020-06-03 20:40:08 -0400

    Created powertools equivalent for a crappy chatbot
    Basically you can do things like add, remove, or show arrays. Not well, of course. But good enough.

--- main.py
+++ main.py
@@ -19,23 +19,33 @@ def create_speech(user_statement, answer, num):
     else:
         return random.choice(data['statement_answer'])
 
-# Does not work
-def modify():
-    command, name, value = user_statement.split()
-    if command == "add":
-        data[name].append(value)
-        print("added",value,"to", name)
-        with open('data.json', 'w') as f:
-            json.dump(data, f, indent=2)
-    elif command == "remove":
-        data[name].pop(value)
-        print("removed",value,"from", name)
-        with open('data.json', 'w') as f:
-            json.dump(data, f, indent=2)
 
-def show():
-    array_name = user_statement.split(" ")
-    print(array_name[1])
+def greeting_speech(user_statement, answer, num):
+    if answer not in user_statement:
+        return None
+    else:
+        return random.choice(data['greeting_answer'])
+
+# def tools():
+#     command, name, value = user_statement.split()
+#     if command == "add":
+#         data[name].append(value)
+#         print("added",value,"to", name)
+#         with open('data.json', 'w') as f:
+#             json.dump(data, f, indent=2)
+#     # This should add a specific item to an array
+#     elif command == "remove":
+#         data[name].pop(value)
+#         print("removed",value,"from", name)
+#         with open('data.json', 'w') as f:
+#             json.dump(data, f, indent=2)
+#     # This should remove a specific item from an array
+#     elif command == "show":
+#             print(data[name])
+#     #This should show a specific array
+#     elif command == "help":
+#             print(data['commands'])
+#     # This will display a list of commands that can be recieved
 
 def speak(text):
     tts = gTTS(text=text, lang="en")
@@ -55,6 +65,12 @@ while True:
             print(response)
             speak(response)
 
+    for i in data['greeting_answer']:
+        response = create_speech(user_statement=user_statement, answer=i, num=num) 
+        if response is not None:
+            print(response)
+            speak(response)
+
     if ("food" or "Food" or "eat" or "Eat") in user_statement:
         print(random.choice(data['food_answer']))
         speak(random.choice(data['food_answer']))
@@ -63,16 +79,8 @@ while True:
         # Error here
         if "end" in user_statement:
             break
-        # This should end the ocnversation and stop the program.
+        # This should end the cnversation and stop the program.
 
-    for i in data['commands']:
-        # make a function here using all of the if statements below 
-        modify()
-
-        if ("show") in user_statement:
-            show()
-            #This should show a specific array
-
-        if ("help") in user_statement:
-            print(data['commands'])
-        # This will display a list of commands that can be recieved
+    # for i in data['commands']:
+    #     # Not sure why this doesn't work 
+    #     tools()
